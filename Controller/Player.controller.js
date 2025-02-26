48166091c50c35f71a0e1714cad8da666def6634
akshitgarg3031 <akshitgarg126@gmail.com>
2025-02-26 22:42:43 +0530

    max team members limit

--- Controller/Player.controller.js
+++ Controller/Player.controller.js
@@ -7,7 +7,7 @@ import { sendEmail } from "../utils/emailService.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
-const maxNumberOfTeamMembers = 3;
+const maxNumberOfTeamMembers = 2;
 
 
 //Create Team
