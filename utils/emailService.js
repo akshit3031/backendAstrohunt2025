79023619b0fe8013770156563d04c25f503e346b
akshitgarg3031 <akshitgarg3031@gmail.com>
2025-10-25 11:35:01 +0530

    changed nodemailer email

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,26 +3,17 @@ import nodemailer from "nodemailer";
 const client = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-        user: "architchhajed6@gmail.com",
-        pass: "smaj ydmv viqz qhrj"
+        user: "astronomy@nitkkr.ac.in",
+        pass: "pdgp yndo zsno hmml"
     }
 });
 
-client.sendMail(
-    {
-        from: "architchhajed6@gmail.com",
-        to: "12213031@nitkkr.ac.in",
-        subject: "Sending",
-        text: "Hello"
-    }
-
-);
 
 
 const sendEmail = async (toEmail, otp) => {
     try{
         const mailOptions = {
-            from: "architchhajed6@gmail.com",
+            from: "astronomy@nitkkr.ac.in",
             to: toEmail,
             subject: "OTP for registration",
             text: `${otp}`
